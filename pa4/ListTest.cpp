925eedba4454247a21beabd7e7bb488add13931d
Kevin Mancio Crawford <kemcrawf@unix3.lt.ucsc.edu>
2021-02-07 16:12:41 -0800

    updated files for make

---
+++ pa4/ListTest.cpp
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Kevin M Crawford, kemcrawf, pa4
+// ListTest.cpp
+// A test client for List ADT
+//-----------------------------------------------------------------------------
+#include<iostream>
+#include<string>
+#include"List.h"
+
+using namespace std;
+
+int main(){
+  return 0;
+}
---
+++ pa4/Shuffle.cpp
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------------
+// Kevin M Crawford, kemcrawf, pa4
+// Shuffle.cpp
+// Uses the list ADT to perform shuffling operations
+// Determines how many shuffles are necessary to bring a List back to
+//  its original order.
+//-----------------------------------------------------------------------------
+#include<iostream>
+#include<string>
+#include"List.h"
+
+using namespace std;
+
+int main(){
+  return 0;
+}
