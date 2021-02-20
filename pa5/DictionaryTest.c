e74201572371ff2eb6c04f0a420cc242ba96ebfc
Kevin Mancio Crawford <kemcrawf@unix6.lt.ucsc.edu>
2021-02-20 12:15:00 -0800

    Added base files for assgn

---
+++ pa5/DictionaryTest.c
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+// Kevin M Crawford, kemcrawf, pa5
+// DictionaryTest.c
+// Test client for Dictionary ADT
+//-----------------------------------------------------------------------------
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include"Dictionary.h"
+
+int main(int argc, char* argv[]){
+  return 0;
+}
---
+++ pa5/Lex.c
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+// Kevin M Crawford, kemcrawf, pa5
+// Lex.c
+// Uses the Dictionary ADT to alphabetize lines in a file
+//-----------------------------------------------------------------------------
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include"Dictionary.h"
+
+int main(int argc, char* argv[]){
+  return 0;
+}
