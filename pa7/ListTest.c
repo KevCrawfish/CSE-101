c914d62a1bd8be961634d31d1da185843a3807a4
Kevin Mancio Crawford <kemcrawf@unix5.lt.ucsc.edu>
2021-03-11 21:29:57 -0800

    Made more complete list test

--- pa7/ListTest.c
+++ pa7/ListTest.c
@@ -8,6 +8,38 @@
 #include"List.h"
 
 int main(int argc, char* argv[]){
-  printf("beep ");
-  return 0;
+  List A = newList();
+  append(A, 5);
+  append(A, 15);
+  append(A, 20);
+  if (length(A) != 3) return 0;
+  prepend(A, 20);
+  prepend(A, 15);
+  prepend(A, 5);
+  moveBack(A);
+  if (index(A) != 5) return 0;
+  List B = copyList(A);
+  moveFront(B);
+  if (get(B) != 5) return 0;
+  if (!equal(A, B)) return 0;
+  moveFront(B);
+  insertAfter(B, 5);
+  moveNext(B);
+  insertAfter(B, 6);
+  moveNext(B);
+  insertBefore(B, 7);
+  deleteFront(B);
+  deleteBack(B);
+  moveFront(B);
+  moveNext(B);
+  if(get(B) != 7 && front(B) != 5 && back(B) != 15) return 0;
+  for(int i = 0; i < 500; i++){
+    insertAfter(B, 6);
+  }
+  freeList(&B);
+  freeList(&A);
+
+//////////////////////
+printf("ok\n");
+return 0;
 }
